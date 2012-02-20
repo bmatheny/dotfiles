711107470d378e7ec241f384705e12f0bfd594a7
Blake Matheny <bmatheny@mobocracy.net>
2012-02-20 10:20:43 -0500

    Updating submodules

--- updateSubmodules.sh
+++ updateSubmodules.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-for i in `find .vim/bundle -name .git|grep -v command-t|sed 's/.git//g'`; do cd $i; git co master; git pull; cd ../../..; done
+for i in `find .vim/bundle -name .git|grep -v command-t|sed 's/.git//g'`; do
+	cd $i;
+	echo "Evaluating `basename $i`"
+	git co master;
+	git pull;
+	cd ../../..;
+done
