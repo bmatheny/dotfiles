b28ad78a2d1bcde7b0570cf3182d95ee29932934
Blake Matheny <bmatheny@mobocracy.net>
2012-02-20 10:28:34 -0500

    Updating submodules

--- updateSubmodules.sh
+++ updateSubmodules.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
-for i in `find .vim/bundle -name .git|grep -v command-t|sed 's/.git//g'`; do
-	cd $i;
-	echo "Evaluating `basename $i`"
-	git co master;
-	git pull;
-	cd ../../..;
+# Where is this running from?
+BASE=$PWD;
+
+for gitdir in `find . -name .git|grep -v command-t|sed 's/.git//g'`; do
+	BASEGIT=`basename $gitdir`
+	if [ "$BASEGIT" == "." ]; then
+		echo "Skipping $BASEGIT";
+	else
+		echo "Evaluating $BASEGIT";
+		cd $gitdir;
+		git co master;
+		git pull;
+		cd $BASE;
+	fi
 done
