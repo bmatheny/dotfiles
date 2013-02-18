26203edfcb8da51ccabe37f9cccc6ab1fe610ddd
Blake Matheny <bmatheny@mobocracy.net>
2013-02-18 12:22:25 -0500

    Updated vim alias, global code alias, java finder

--- .zshplugins/aliases.zsh
+++ .zshplugins/aliases.zsh
@@ -5,15 +5,22 @@ alias lsize='ls --color=always -sh'
 alias more='less'
 
 # vim stuff
-alias vim="vim -o $@"
+alias vim="nocorrect vim -o $@"
 alias vtree="vim +NERDTree +TagbarOpen"
 alias vi=vim
 alias v="f -t -e vim"
+
+# Ignore helpers
+local IGNORES='\\.svn\|\\.git'
+local OBJS='\\.class$'
+alias -g XS="| egrep -v ${IGNORES}\|${OBJS}"
+alias -g XSF=" -type f | egrep -v ${IGNORES}\|${OBJS}"
+alias -g XSS="| egrep -v ${IGNORES}\|${OBJS}\|/target/"
+alias -g XSSF=" -type f | egrep -v ${IGNORES}\|${OBJS}\|/target/"
 
 # dev stuff
 alias diff=colordiff
 alias tmux='tmux -2'
 alias -g ff='ack -g'
-alias -g XS='| egrep -v \\.svn\|\\.class$\|\\.git'
 alias gd='git diff'
 compdef _git gd=git-diff
