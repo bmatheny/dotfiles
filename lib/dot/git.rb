5d3b0b9c42affd564199afcc8268a0fb22cffb78
Blake Matheny <bmatheny@mobocracy.net>
2022-09-08 19:54:21 -0500

    Getting rid of old/unused binaries and unused code

--- lib/dot/git.rb
+++
@@ -1,8 +0,0 @@
-require 'dot/command/runner'
-
-module Dot; module Git
-  # Execute a git command
-  def self.git cfg, cmd, force = false
-    Dot::Command::Runner.run(cfg, "#{cfg.gitcmd} #{cmd}", force)
-  end
-end; end # module Dot; module Git
