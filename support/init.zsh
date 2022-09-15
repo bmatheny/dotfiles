9e5173b76c5315ab9fc59cb9e6b5da80f2c0a647
Blake Matheny <bmatheny@mobocracy.net>
2022-09-15 15:29:05 -0500

    scripts: shared code, user specified brews
    This commit adds two things; shared code for scripts, and the ability for users
    to provide their own home brew file.
    
    For shared code, it turned out it's challenging to allow for code reuse from
    within the '.chezmoiscripts' directory for a variety of reasons. What ended up
    making the most sense given the issues I ran into was; code is captured in a
    support directory, and scripts wanting to use that code follows the
    "initialize_support" strategy found in the brews scripts. Essentially most of
    the magic here is due to me wanting to allow for chezmoi templates to be run
    both as a standalone script for testing, and also as a normal chezmoi script.
    
    For users providing their own home brew file, just put a
    ~/.config/bmatheny/packages.yaml file in place with your packages you would like
    installed. I'll almost immediately move this to Brewfile format for a few
    reasons, but that's ok since nobody uses my repo anyways.

---
+++ support/init.zsh
@@ -0,0 +1,39 @@
+# vim: set ft=zsh:
+
+SOURCEDIR=""
+
+function error_fn() {
+  echo "Exiting with error: '$1'"
+  exit 1
+}
+
+function init_source() {
+  SOURCEDIR="$1"
+  if [ ! -d "${SOURCEDIR}" ]; then
+    echo "Specified directory is not a directory or does not exist: '${SOURCEDIR}'"
+    exit 1
+  fi
+}
+
+function load_support_script() {
+  local readonly script="$1"
+  if [ -z "${SOURCEDIR}" ]; then
+    echo "Please call init_source first"
+    exit 1
+  fi
+  source "${SOURCEDIR}/support/${script}"
+}
+
+function brew_source() {
+  if (( ! $+commands[brew] )); then
+    homebrew_locations=(
+      "${HOME}/homebrew"
+      "/usr/local")
+    for location in $homebrew_locations; do
+      if [[ -d "$location/bin" && -x "$location/bin/brew" ]]; then
+        eval "$(${location}/bin/brew shellenv)"
+        break
+      fi
+    done
+  fi
+}
