01d107b4e7abced8d29f88f52adb204ddf882043
Blake Matheny <bmatheny@mobocracy.net>
2022-09-15 16:38:27 -0500

    scripts: osx customization, brewfile support
    I have been borrowing this osx script (bin/osx) for customizing my mac, and
    bringing it between machines, forever. Adding it here for posterity.
    
    Moved from yaml to Brewfile for brew package management which resulted in lots
    of awesomeness.

--- support/brews.zsh
+++ support/brews.zsh
@@ -3,7 +3,6 @@
 function _check_cmds() {
   (( $+commands[brew] )) || error_fn "Could not find homebrew"
   (( $+commands[chezmoi] )) || error_fn "Could not find chezmoi"
-  (( $+commands[shyaml] )) || error_fn "Could not find shyaml"
 }
 
 function _check_file() {
@@ -19,17 +18,6 @@ function _check_file() {
   fi
 }
 
-function _install_brews_from_file() {
-  local file="$1"
-  for group in $(cat "${file}" | shyaml -q -y keys brew); do
-    brewfile=""
-    for brew in $(cat "${file}" | shyaml get-values "brew.${group}"); do
-      brewfile="${brewfile}\nbrew \"${brew}\""
-    done
-    echo $brewfile | brew bundle --quiet --file=-
-  done
-}
-
 function brew_install() {
   local readonly file="$1"
   local readonly required=$2
@@ -37,5 +25,5 @@ function brew_install() {
   brew_source
   _check_cmds
   _check_file "${file}" $required
-  _install_brews_from_file "${file}"
+  brew bundle --quiet --file="${file}"
 }
