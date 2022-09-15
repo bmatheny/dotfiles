a5ffb0c6de65cf3687e7d01160bcb4fdf2434cad
Blake Matheny <bmatheny@mobocracy.net>
2022-09-14 20:35:34 -0500

    scripts: Only update software on list change
    Previously if anything in packages.yaml or ~/.config/bmatheny/packages.yaml
    changed, it would trigger a run of all software install scripts.
    
    With this change, we generate hashes specifically for the relevant portion of
    the config. So a run of the software installer is only triggered if that
    software installers list changed.

--- support/checksum.sh
+++ support/checksum.sh
@@ -1,23 +1,41 @@
 #!/usr/bin/env zsh
 # vim: set ft=zsh:
 
-# Usage: checksum file
+# Usage: checksum file or part of file if yaml
 # Prints sha or default value
 
 function usage {
-  echo "Usage: $0 file $1"
+  echo "Usage: $0 file key"
+  if [[ ! -z "$1" ]]; then
+    echo "error was: $1"
+  fi
   exit 1
 }
 
 file=$1
-if [ -z "$file" ]; then
+if [[ -z "$file" ]]; then
   usage
 fi
 
 (( $+commands[shasum] )) || usage "Failed to find shasum program"
 
+key=$2
+use_yaml=0
+# If the user specified a key in the file
+if [[ ! -z "$key" ]]; then
+  # And we have shyaml installed
+  if (( $+commands[shyaml] )); then
+    # Then we can use yaml
+    use_yaml=1
+  fi
+fi
+
 if [ -f "$file" -a -r "$file" ]; then
-  shasum -a 256 "$file" | sed 's/ .*//'
+  if [[ $use_yaml -eq 1 ]]; then
+    cat "$file" | shyaml -q get-value "$key" | shasum -a 256 | sed 's/ .*//'
+  else
+    shasum -a 256 "$file" | sed 's/ .*//'
+  fi
 else
   echo "NO SUCH FILE"
 fi
