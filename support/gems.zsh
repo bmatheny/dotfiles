0c00fef6ab1c6bbeaba4fc739fb8eb48484c7691
Blake Matheny <bmatheny@mobocracy.net>
2024-12-12 19:30:47 -0600

    Linux: let gem install in user local location

--- support/gems.zsh
+++ support/gems.zsh
@@ -3,6 +3,7 @@
 function gem_install() {
   local file="$1"
   local required=$2
+  local install_user=0
 
   brew_source
   # If we have homebrew, assume we are using a homebrew installed ruby and gem binary
@@ -15,13 +16,22 @@ function gem_install() {
     fi
   else
     path=("$(gem environment user_gemhome)/bin" $path)
+    install_user=1
   fi
   check_cmds "ruby"
 
   check_file "${file}" $required
   if (( ! $+commands[bundle] )); then
     echo "Installing bundler to process Gemfile '${file}'"
-    gem install bundler
+    if ((install_user)); then
+      gem install --user-install bundler
+    else
+      gem install bundler
+    fi
+  fi
+
+  if ((install_user)); then
+    bundle config set --local path $(gem environment user_gemhome)
   fi
   bundle install --gemfile="${file}"
 }
